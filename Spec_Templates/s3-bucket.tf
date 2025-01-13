b9b43dd3a2a7f26347f6249a48066ad9adb1058c
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2025-01-13 14:37:38 +0000

    Update s3-bucket.tf

--- Spec_Templates/s3-bucket.tf
+++ Spec_Templates/s3-bucket.tf
@@ -9,6 +9,12 @@ provider "aws" {
     ec2_metadata_service_endpoint_mode = "IPv4"
 }
 
+#======= Variables =========#
+
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+
 #===========S3-Bucket ======#
 
 resource "aws_s3_bucket" "s3bucket" {
