0d1f1d9b423837dc5c1d3615dff8a033d91e8a26
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2025-01-17 12:10:55 +0000

    Update locals.tf

--- AWS_IAM/locals.tf
+++ AWS_IAM/locals.tf
@@ -3,36 +3,36 @@ locals {
 # Common tags to be assigned to all AWS resources and Azure Resource Groups
 
   default_tags = {
-    Name = "<%=instance.displayName%>"
-    PrimaryContact = "<%=instance.metadata.PrimaryContact%>"
-    SecondaryContact = "<%= instance.metadata.SecondaryContact%>"
-    NotificationPDL = "<%= instance.metadata.NotificationPDL%>"
-    ApplicationName = "<%= instance.metadata.AppInstanceID%>"
-    Environment = "<%= instance.metadata.Environment%>"
-    ITOwnerEmail = "<%= instance.metadata.NotificationPDL%>"
+    Name = "test"
+    PrimaryContact = "test"
+    SecondaryContact = "test"
+    NotificationPDL = "test"
+    ApplicationName = "test"
+    Environment = "test"
+    ITOwnerEmail = "test"
   }
 
 # Common tags to be assigned to all Azure resources (App Name and ITOwner are inherited from the resource group level
 
   azure_resource_default_tags = {
-    Name = "<%=instance.displayName%>"
-    PrimaryContact = "<%=instance.metadata.PrimaryContact%>"
-    SecondaryContact = "<%= instance.metadata.SecondaryContact%>"
-    NotificationPDL = "<%= instance.metadata.NotificationPDL%>"
-    Environment = "<%= instance.metadata.Environment%>"
+    Name = "test"
+    PrimaryContact = "test"
+    SecondaryContact = "test"
+    NotificationPDL = "test"
+    Environment = "test"
   }
 
   instance_options = {
-    name = "<%=instance.displayName%>"
-    domainName = "<%=instance.domainName%>"
-    securitygroups = "<%= instance.metadata.CloudSecurityGroup%>"
+    name = "test"
+    domainName = "test"
+    securitygroups = "test"
   }
 
   cloud_options = {
-    region = "<%= instance.metadata.CloudRegion%>"
-    account = "<%= instance.metadata.CloudAccount%>"
-    rg_name = "<%= instance.metadata.ResourceGroupName%>"
-    iam_user_name = "<%= instance.metadata.IAMUserName%>"
-    azure_sku = "<%= instance.metadata.AzureSKU%>"
+    region = "test"
+    account = "test"
+    rg_name = "test"
+    iam_user_name = "test"
+    azure_sku = "test"
   }
 }
