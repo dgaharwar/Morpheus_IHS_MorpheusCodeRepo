1aa3b30124784289be7870a4488fce71d411d4cc
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2025-01-17 14:43:57 +0000

    Update locals.tf

--- AWS_IAM/locals.tf
+++ AWS_IAM/locals.tf
@@ -2,37 +2,37 @@ locals {
 
 # Common tags to be assigned to all AWS resources and Azure Resource Groups
 
-  common_tags = {
-    Name = "test"
-    PrimaryContact = "test"
-    SecondaryContact = "test"
-    NotificationPDL = "test"
-    ApplicationName = "test"
-    Environment = "test"
-    ITOwnerEmail = "test"
+  default_tags = {
+    Name = "<%=instance.displayName%>"
+    PrimaryContact = "<%=instance.metadata.PrimaryContact%>"
+    SecondaryContact = "<%= instance.metadata.SecondaryContact%>"
+    NotificationPDL = "<%= instance.metadata.NotificationPDL%>"
+    ApplicationName = "<%= instance.metadata.AppInstanceID%>"
+    Environment = "<%= instance.metadata.Environment%>"
+    ITOwnerEmail = "<%= instance.metadata.NotificationPDL%>"
   }
 
 # Common tags to be assigned to all Azure resources (App Name and ITOwner are inherited from the resource group level
 
   azure_resource_default_tags = {
-    Name = "test"
-    PrimaryContact = "test"
-    SecondaryContact = "test"
-    NotificationPDL = "test"
-    Environment = "test"
+    Name = "<%=instance.displayName%>"
+    PrimaryContact = "<%=instance.metadata.PrimaryContact%>"
+    SecondaryContact = "<%= instance.metadata.SecondaryContact%>"
+    NotificationPDL = "<%= instance.metadata.NotificationPDL%>"
+    Environment = "<%= instance.metadata.Environment%>"
   }
 
   instance_options = {
-    name = "test"
-    domainName = "test"
-    securitygroups = "test"
+    name = "<%=instance.displayName%>"
+    domainName = "<%=instance.domainName%>"
+    securitygroups = "<%= instance.metadata.CloudSecurityGroup%>"
   }
 
   cloud_options = {
-    region = "test"
-    account = "test"
-    rg_name = "test"
-    iam_user_name = "test"
-    azure_sku = "test"
+    region = "<%= instance.metadata.CloudRegion%>"
+    account = "<%= instance.metadata.CloudAccount%>"
+    rg_name = "<%= instance.metadata.ResourceGroupName%>"
+    iam_user_name = "<%= instance.metadata.IAMUserName%>"
+    azure_sku = "<%= instance.metadata.AzureSKU%>"
   }
 }
